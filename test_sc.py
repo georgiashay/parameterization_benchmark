2d2d2703639e65884993663ece3aaa4cba846c02
Oded Stein <ostein@mit.edu>
2022-03-11 17:18:21 -0500

    prettier plots

--- test_sc.py
+++ test_sc.py
@@ -18,10 +18,5 @@ from utilities.overlap_area import get_overlap_area
 from utilities.resolution import get_resolution
 from selected_plots import selected_plots
 
-V,Vt,_,F,Ft,_ = igl.read_obj('/Users/ostein/Documents/university/MIT/opensource/designs/meshes/objects/mountain/mountain.obj')
-Vt = V[:,0:2]
-Ft = F.copy()
-
-
-selected_plots('/Users/ostein/Desktop/distortion_characteristics.csv')
+selected_plots('/home/ostein/Desktop/f1.csv', '/home/ostein/Desktop/f2.csv')
 
