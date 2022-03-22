9ae90a109d86a7687786fffab21d1b692f2ae833
Georgia Shay <georgiashay@gmail.com>
2022-03-22 14:29:36 -0400

    Change UV cut match formula

--- test/test_artist_cut_match_uv.py
+++ test/test_artist_cut_match_uv.py
@@ -20,7 +20,7 @@ def test_artist_cut_match_uv_cube():
     
     artist_cut_match_uv = get_artist_cut_match_uv(uv_o, ftc_o, uv, ftc)
     
-    assert artist_cut_match_uv == pytest.approx(2/math.sqrt(6))
+    assert artist_cut_match_uv == pytest.approx(4/math.sqrt(6))
     
 def test_artist_cut_match_uv_icosa():
     ofpath = os.path.join(fixture_dir, "icosahedron_hemi.obj")
@@ -32,9 +32,9 @@ def test_artist_cut_match_uv_icosa():
     artist_cut_match_uv = get_artist_cut_match_uv(uv_o, ftc_o, uv, ftc)
     
     scaled_edge_length_measure = math.sqrt((4/math.sqrt(3)) * (1/20))
-    measure_length = 11 * scaled_edge_length_measure
+    measure_length = 22 * scaled_edge_length_measure
     
     scaled_edge_length_artist = 0.447785
-    artist_length = 6 * scaled_edge_length_artist
+    artist_length = 12 * scaled_edge_length_artist
   
     assert artist_cut_match_uv == pytest.approx(measure_length - artist_length)
