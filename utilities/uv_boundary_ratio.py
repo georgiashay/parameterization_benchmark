56eaf350d6a2616c980e73faeda96d8329ee1d45
Georgia Shay <georgiashay@gmail.com>
2022-03-23 19:22:00 -0400

    Test uv boundary ratio

--- utilities/uv_boundary_ratio.py
+++ utilities/uv_boundary_ratio.py
@@ -1,3 +1,6 @@
+import igl
+import numpy as np
+
 def get_uv_boundary_length(uv, ftc):
     boundary_uv_edges = uv[igl.boundary_facets(ftc)]
     boundary_length = np.sum(np.linalg.norm(boundary_uv_edges[:,0,:] - boundary_uv_edges[:,1,:], axis=1))
