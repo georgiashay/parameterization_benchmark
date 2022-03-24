bf6efb41d58a085a9f4f02f9f76683237b8c254a
Georgia Shay <georgiashay@gmail.com>
2022-03-24 14:30:06 -0400

    Use strtree for overlap area

--- utilities/overlap_area.py
+++ utilities/overlap_area.py
@@ -1,5 +1,5 @@
 import numpy as np
-from shapely import geometry, ops
+from shapely import geometry, ops, strtree
 
 def get_overlap_area(ftc, uv, singular_values):
     magnification = singular_values[:, 0] * singular_values[:, 1]
@@ -13,11 +13,14 @@ def get_overlap_area(ftc, uv, singular_values):
         point_list = [tuple(row) for row in tri]
         triangle = geometry.Polygon(point_list)
         triangles.append(triangle)
+        
+    triangle_tree = strtree.STRtree(triangles)
 
     overlap_area = 0
     for i, tri1 in enumerate(triangles):
-        for j, tri2 in enumerate(triangles):
-            if i != j:
+        other_triangles = triangle_tree.query(tri1)
+        for tri2 in other_triangles:
+            if tri1 != tri2:
                 intersect_area = tri1.intersection(tri2).area
                 overlap_area += intersect_area/magnification[i]
                 
