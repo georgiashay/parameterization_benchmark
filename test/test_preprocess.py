d8cc36b138de16861168296487c76d97a6441a73
Oded Stein <ostein@mit.edu>
2022-08-02 18:13:00 -0400

    fix preprocess unit test

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -17,7 +17,7 @@ def test_preprocess_square():
     assert v_i == pytest.approx(v)
     assert uv_i == pytest.approx(uv)
     assert v == pytest.approx(np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]))
-    assert uv == pytest.approx(np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]))
+    assert uv == pytest.approx(np.array([[0, 0], [1, 0], [1, 1], [0, 1]]))
     
 def test_preprocess_square_scaled():
     fpath = os.path.join(fixture_dir, "square_scaled.obj")
@@ -26,9 +26,9 @@ def test_preprocess_square_scaled():
     assert mesh_areas == pytest.approx(np.array([[0.5, 0.5]]))
     assert uv_areas == pytest.approx(np.array([[0.5, 0.5]]))
     assert v_i == pytest.approx(np.array([[-5, -5, 0], [5, -5, 0], [5, 5, 0], [-5, 5, 0]]))
-    assert uv_i == pytest.approx(np.array([[0, 0, 0], [2, 0, 0], [2, 2, 0], [0, 2, 0]]))
+    assert uv_i == pytest.approx(np.array([[0, 0], [2, 0], [2, 2], [0, 2]]))
     assert v == pytest.approx(np.array([[-0.5, -0.5, 0], [0.5, -0.5, 0], [0.5, 0.5, 0], [-0.5, 0.5, 0]]))
-    assert uv == pytest.approx(np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]))
+    assert uv == pytest.approx(np.array([[0, 0], [1, 0], [1, 1], [0, 1]]))
     
 def test_preprocess_square_stretched():
     fpath = os.path.join(fixture_dir, "square_stretched.obj")
@@ -37,10 +37,10 @@ def test_preprocess_square_stretched():
     assert mesh_areas == pytest.approx(np.array([[0.5, 0.5]]))
     assert uv_areas == pytest.approx(np.array([[0.5, 0.5]]))
     assert v_i == pytest.approx(np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]))
-    assert uv_i == pytest.approx(np.array([[0, 0, 0], [2, 0, 0], [2, 1, 0], [0, 1, 0]]))
+    assert uv_i == pytest.approx(np.array([[0, 0], [2, 0], [2, 1], [0, 1]]))
     assert v == pytest.approx(np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]))
-    assert uv == pytest.approx(np.array([[0, 0, 0], [math.sqrt(2), 0, 0], 
-                                         [math.sqrt(2), 1/math.sqrt(2), 0], [0, 1/math.sqrt(2), 0]]))
+    assert uv == pytest.approx(np.array([[0, 0], [math.sqrt(2), 0], 
+                                         [math.sqrt(2), 1/math.sqrt(2)], [0, 1/math.sqrt(2)]]))
     
 def test_preprocess_grid_shift():
     fpath = os.path.join(fixture_dir, "grid_shift.obj")
@@ -50,15 +50,15 @@ def test_preprocess_grid_shift():
     assert uv_areas == pytest.approx(np.array([[0.1875, 0.1875, 0.0625, 0.0625, 0.1875, 0.1875, 0.0625, 0.0625]]))
     assert v_i == pytest.approx(np.array([[0, 0, 0], [1, 0, 0], [2, 0, 0], [0, 1, 0], [1, 1, 0], 
                                           [2, 1, 0], [0, 2, 0], [1, 2, 0], [2, 2, 0]]))
-    assert uv_i == pytest.approx(np.array([[0, 0, 0], [3, 0, 0], [4, 0, 0], [0, 1, 0], [3, 1, 0], 
-                                           [4, 1, 0], [0, 2, 0], [3, 2, 0], [4, 2, 0]]))
+    assert uv_i == pytest.approx(np.array([[0, 0], [3, 0], [4, 0], [0, 1], [3, 1], 
+                                           [4, 1], [0, 2], [3, 2], [4, 2]]))
     assert v == pytest.approx(np.array([[0, 0, 0], [0.5, 0, 0], [1, 0, 0], [0, 0.5, 0], [0.5, 0.5, 0],
                                         [1, 0.5, 0], [0, 1, 0], [0.5, 1, 0], [1, 1, 0]]))
-    assert uv == pytest.approx(np.array([[0, 0, 0], [3/math.sqrt(8), 0, 0], [4/math.sqrt(8), 0, 0], 
-                                         [0, 1/math.sqrt(8), 0], [3/math.sqrt(8), 1/math.sqrt(8), 0],
-                                         [4/math.sqrt(8), 1/math.sqrt(8), 0], [0, 2/math.sqrt(8), 0], 
-                                         [3/math.sqrt(8), 2/math.sqrt(8), 0], 
-                                         [4/math.sqrt(8), 2/math.sqrt(8), 0]]))
+    assert uv == pytest.approx(np.array([[0, 0], [3/math.sqrt(8), 0], [4/math.sqrt(8), 0], 
+                                         [0, 1/math.sqrt(8)], [3/math.sqrt(8), 1/math.sqrt(8)],
+                                         [4/math.sqrt(8), 1/math.sqrt(8)], [0, 2/math.sqrt(8)], 
+                                         [3/math.sqrt(8), 2/math.sqrt(8)], 
+                                         [4/math.sqrt(8), 2/math.sqrt(8)]]))
     
 def test_preprocess_no_uv_area():
     fpath = os.path.join(fixture_dir, "flat_triangle.obj")
